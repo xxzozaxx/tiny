30957e13f5564984bfc010068a373ff87f478e93
Ömer Sinan Ağacan <omeragacan@gmail.com>
2017-10-21 08:10:49 +0300

    Add precedences to nicks

--- src/tui/name_list.rs
+++ src/tui/name_list.rs
@@ -6,11 +6,39 @@ use tui::termbox;
 use tui::widget::WidgetRet;
 
 pub struct NameList {
-    names: Vec<String>,
+    names: Vec<Name>,
     scroll: i32,
     selection: i32,
     height: i32,
     width: i32,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Name {
+    draw_str: String,
+    presence: Option<String>,
+    name: String,
+}
+
+impl Name {
+    fn new(name: String, presence: Option<String>) -> Name {
+        let draw_str = match presence {
+            None =>
+                name.clone(),
+            Some(ref p) =>
+                format!("[{}]{}", p, name),
+        };
+        Name { draw_str, name, presence }
+    }
+
+    fn draw(&self, tb: &mut Termbox, colors: &Colors, pos_x: i32, pos_y: i32, width: i32) {
+        termbox::print_chars(
+            tb,
+            pos_x,
+            pos_y,
+            colors.user_msg,
+            self.draw_str.chars().take(width as usize));
+    }
 }
 
 impl NameList {
@@ -38,12 +66,7 @@ impl NameList {
         let range_end = ::std::cmp::min((self.scroll + self.height) as usize, self.names.len());
         let names_range = &self.names[range_begin .. range_end];
         for (name_idx, name) in names_range.iter().enumerate() {
-            termbox::print_chars(
-                tb,
-                pos_x,
-                pos_y + name_idx as i32,
-                colors.user_msg,
-                name.chars().take(self.width as usize));
+            name.draw(tb, colors, pos_x, pos_y + name_idx as i32, self.width);
         }
     }
 
@@ -53,14 +76,23 @@ impl NameList {
     }
 
     pub fn join(&mut self, nick: String) {
-        if let Err(idx) = self.names.binary_search(&nick) {
-            self.names.insert(idx, nick);
+        let name = Name::new(nick, None);
+        if let Err(idx) = self.names.binary_search(&name) {
+            self.names.insert(idx, name);
         }
     }
 
     pub fn part(&mut self, nick: &str) {
-        if let Ok(idx) = self.names.binary_search_by(|s| s.as_str().cmp(nick)) {
+        if let Ok(idx) = self.names.binary_search_by(|s| s.name.as_str().cmp(nick)) {
             self.names.remove(idx);
+        }
+    }
+
+    pub fn set_precedence(&mut self, nick: &str, presence: &str) {
+        self.part(nick);
+        let name = Name::new(nick.to_owned(), Some(presence.to_owned()));
+        if let Err(idx) = self.names.binary_search(&name) {
+            self.names.insert(idx, name);
         }
     }
 }
